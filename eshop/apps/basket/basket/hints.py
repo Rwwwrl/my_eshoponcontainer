2c249c6a4febf0d37cbf91bf06640accae6f13bb
Rwwwrl <forgithublesh@yandex.ru>
2024-06-30 14:11:28 +0300

    [ESHOP-68] feat basket hints.py заменил int, float на PositiveInt, PositiveFloat

--- eshop/apps/basket/basket/hints.py
+++ eshop/apps/basket/basket/hints.py
@@ -1,14 +1,14 @@
 from typing import NewType
 
-# TODO в задаче ESHOP-48 это заменится на pydantic.FieldTypes
+from pydantic.types import PositiveFloat, PositiveInt
 
-BuyerId = NewType('BuyerId', int)
+BuyerId = NewType('BuyerId', PositiveInt)
 CustomerBasketPK = NewType('CustomerBasketPK', BuyerId)
 
-BasketItemId = NewType('BasketItemId', int)
-ProductId = NewType('ProductId', int)
+BasketItemId = NewType('BasketItemId', PositiveInt)
+ProductId = NewType('ProductId', PositiveInt)
 
 ProductName = NewType('ProductName', str)
-Price = NewType('Price', float)
-Quantity = NewType('Quantity', int)
+Price = NewType('Price', PositiveFloat)
+Quantity = NewType('Quantity', PositiveInt)
 PictureUrl = NewType('PictureUrl', str)
